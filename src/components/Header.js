58f910bd9e3108fabc07a8638db35fdb5fd5678e
Ashish Mainali <19223717@brookes.com>
2023-01-23 18:56:11 +0000

    - Update implement react router - Update add product screen page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header(params) {
     return (
@@ -7,16 +8,22 @@ function Header(params) {
         <header>
           <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
             <Container>
-              <Navbar.Brand href="/">E-commerce</Navbar.Brand>
+              <LinkContainer to="/">
+                <Navbar.Brand>E-commerce</Navbar.Brand>
+              </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/cart">
-                    <i className="fas fa-shopping-cart"></i>Cart
-                  </Nav.Link>
-                  <Nav.Link href="/login">
-                    <i className="fas fa-user"></i>
-                  </Nav.Link>
+                  <LinkContainer to="/cart">
+                    <Nav.Link>
+                      <i className="fas fa-shopping-cart"></i>&nbsp;Cart
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <i className="fas fa-user"></i>&nbsp;Login
+                    </Nav.Link>
+                  </LinkContainer>
                 </Nav>
               </Navbar.Collapse>
             </Container>
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductScreen() {
+  return (
+    <div>
+      <h2>Product Page.</h2>
+    </div>
+  );
+}
+
+export default ProductScreen;
