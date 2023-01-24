39b619b1fc48c041d953031f2647bc4ec2e1e61a
Ashish Mainali <19223717@brookes.com>
2023-01-24 11:00:57 +0000

    - Update add reducers and redux in home screen - Update add provider in index

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
-import axios from 'axios';
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList
 
   useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products/")
-      setProducts(data)
-    }
-    fetchProducts();
-  },[])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Latest Products</h1>
+      {loading ? <h2>Loading..</h2>
+        : error ? <h3>{error}</h3>
+        :
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={3}>
@@ -24,6 +26,7 @@ function HomeScreen() {
           </Col>
         ))}
       </Row>
+    }
     </div>
   );
 }
