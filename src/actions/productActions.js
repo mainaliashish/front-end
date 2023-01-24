0a807f4c51ca8b73484e5cc11602bc43fea0d89d
Ashish Mainali <19223717@brookes.com>
2023-01-24 11:00:06 +0000

    - Update add product adction - Update add product reducers

---
+++ src/actions/productActions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+
+export const listProducts = (keyword = "") => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get(`/api/products${keyword}`);
+
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
