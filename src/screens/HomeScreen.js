4916246d813849a35e9f9a298bdccc2370fef2f3
Ashish Mainali <19223717@brookes.com>
2023-01-24 11:07:34 +0000

    - Update add loader and message component - Update home page with loader and message

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 function HomeScreen() {
   const dispatch = useDispatch();
@@ -16,17 +18,19 @@ function HomeScreen() {
   return (
     <div>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading..</h2>
-        : error ? <h3>{error}</h3>
-        :
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
-    }
+      {loading ? (
+        <Loader />
+      ) : error ? (
+          <Message variant="danger">{ error }</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
