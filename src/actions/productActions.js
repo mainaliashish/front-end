0d5e83c2cd58a421c9250b4631a02f9b14252727
Ashish Mainali <19223717@brookes.com>
2023-01-27 10:11:21 +0000

    - Update add product actions js

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const listProducts = (keyword = "") => async (dispatch) => {
@@ -25,3 +28,25 @@ export const listProducts = (keyword = "") => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/product/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
